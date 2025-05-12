f95088e78c69fe4e596471e9bda5b7e773145e79
Hồng Nhung <20010877@st.phenikaa-uni.edu.vn>
2025-05-12 21:35:57 +0700

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE login (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(40) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
+    role VARCHAR(20) NOT NULL CHECK (role IN ('Admin', 'Student', 'Lecturer'))
 );
 
 CREATE TABLE persistent_logins (
