dea4e5989ddb24f1f8cfc4f59b182380e1958ba1
Hồng Nhung <20010877@st.phenikaa-uni.edu.vn>
2025-05-13 04:31:35 +0700

    Delete src/main/java/com/university/management directory

--- src/main/java/com/university/management/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.entity;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface PaymentService {
-    Page<Payment> findPaymentsByStatus(String status, Pageable pageable);
-    void updatePaymentStatus(Integer id, Payment.Status status, String approverUsername);
-}
